b7a887b7da12537a267d726632960fdadd3fccde
Daniel Sousa <sousa.dfs@gmail.com>
2016-10-14 22:42:28 -0300

    Adiciona javascript para calendário e tabela

---
+++ Client/js/tabela.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('.table-data').DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": true,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false
+    });
+});
