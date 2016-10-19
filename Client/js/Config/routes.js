162eab9271ffe69341d5b0192ec87637fab5782e
Daniel Sousa <sousa.dfs@gmail.com>
2016-10-19 01:13:41 -0200

    Adiciona página e rota: Criar Recurso e Criar Questão

--- Client/js/Config/routes.js
+++ Client/js/Config/routes.js
@@ -11,20 +11,116 @@ app.config(function ($routeProvider, $locationProvider) {
             templateUrl: 'pages/login.html'
         })
 
+        /******************
+         *    Questões    *
+         ******************/
+
+        .when('/questao/criar/objetiva', {
+            templateUrl: 'pages/questoes/criar/objetiva.html'
+        })
+
+        .when('/questao/criar/subjetiva', {
+            templateUrl: 'pages/questoes/criar/subjetiva.html'
+        })
+
+        .when('/questao/:id', {
+            templateUrl: 'pages/questoes/visualizar.html'
+        })
+
         .when('/questoes', {
             templateUrl: 'pages/questoes/listar.html'
         })
+
+        .when('/questoes/aguardando', {
+            templateUrl: 'pages/questoes/listar.html'
+        })
+
+        .when('/questoes/aceitas', {
+            templateUrl: 'pages/questoes/listar.html'
+        })
+
+        .when('/questoes/recusadas', {
+            templateUrl: 'pages/questoes/listar.html'
+        })
+
+        .when('/questoes/rascunhos', {
+            templateUrl: 'pages/questoes/listar.html'
+        })
+
+        /******************
+         *     Provas     *
+         ******************/
 
         .when('/provas', {
             templateUrl: 'pages/provas/listar.html'
         })
 
+        .when('/prova/:id', {
+            templateUrl: 'pages/provas/visualizar.html'
+        })
+
+        .when('/provas/aguardando', {
+            templateUrl: 'pages/provas/listar.html'
+        })
+
+        .when('/provas/aceitas', {
+            templateUrl: 'pages/provas/listar.html'
+        })
+
+        .when('/provas/recusadas', {
+            templateUrl: 'pages/provas/listar.html'
+        })
+
+        .when('/provas/rascunhos', {
+            templateUrl: 'pages/provas/listar.html'
+        })
+
+        .when('/provas/imprimir', {
+            templateUrl: 'pages/provas/listar.html'
+        })
+
+        .when('/provas/digitalizar', {
+            templateUrl: 'pages/provas/listar.html'
+        })
+
+        /******************
+         *     Notas      *
+         ******************/
+
         .when('/notas', {
             templateUrl: 'pages/notas/listar.html'
         })
 
+        .when('/notas/:disciplina', {
+            templateUrl: 'pages/notas/listar.html'
+        })
+
+        /******************
+         *    Recursos    *
+         ******************/
+
         .when('/recursos', {
             templateUrl: 'pages/recursos/listar.html'
+        })
+
+        .when('/recurso/criar', {
+            templateUrl: 'pages/recursos/criar.html'
+        })
+
+        .when('/recursos/aguardando', {
+            templateUrl: 'pages/recursos/listar.html'
+        })
+
+        .when('/recursos/aceitos', {
+            templateUrl: 'pages/recursos/listar.html'
+        })
+
+        .when('/recursos/recusados', {
+            templateUrl: 'pages/recursos/listar.html'
+        })
+
+        .when('/recurso/:id', {
+            templateUrl: 'pages/recursos/visualizar.html'
         })
 
         .otherwise({templateUrl: 'pages/404.html'});
---
+++ Client/js/editor.js
@@ -0,0 +1,6 @@
+$(function () {
+    CKEDITOR.replace('editor', {
+        uiColor: '#f9f9f9',
+        height: 300
+    });
+});
