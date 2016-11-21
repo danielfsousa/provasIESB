699c0867c4f373bf9f2b77659ee21d4487d29b8a
Ludwig <ludmasca@hotmail.com>
2016-11-20 23:01:54 -0200

    questoes

--- Server/database/factories/ModelFactory.php
+++ Server/database/factories/ModelFactory.php
@@ -23,3 +23,31 @@ $factory->define(App\Recurso::class, function (Faker\Generator $faker) {
         'estado_id' => 1,
     ];
 });
+
+$factory->define(App\Objetiva::class, function (Faker\Generator $faker) {
+    return [
+        'disciplina_id' => 1,
+        'autor_id' => 2,
+        'tags' => 'tag',
+        'estado_id' => collect([1, 2, 3])->random(),
+        'dificuldade' => collect(['Alta', 'Média', 'Baixa'])->random(),
+        'enunciado' => $faker->text,
+        'alternativa_a' => $faker->sentence(5),
+        'alternativa_b' => $faker->sentence(5),
+        'alternativa_c' => $faker->sentence(5),
+        'alternativa_d' => $faker->sentence(5),
+        'alternativa_e' => $faker->sentence(5),
+        'alternativa_correta' => collect(['a', 'b', 'c', 'd', 'e'])->random(),
+    ];
+});
+
+$factory->define(App\Subjetiva::class, function (Faker\Generator $faker) {
+    return [
+        'disciplina_id' => 1,
+        'autor_id' => 2,
+        'tags' => 'tag',
+        'estado_id' => collect([1, 2, 3])->random(),
+        'dificuldade' => collect(['Alta', 'Média', 'Baixa'])->random(),
+        'enunciado' => $faker->text,
+    ];
+});
--- Server/database/seeds/DatabaseSeeder.php
+++ Server/database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsuariosSeeder::class);
         $this->call(RecursosSeeder::class);
+        $this->call(QuestoesSeeder::class);
     }
 }
