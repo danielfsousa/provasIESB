e908b775fd1f0970d01f692f2de9ab4ab1b25079
Daniel Sousa <sousa.dfs@gmail.com>
2017-07-25 03:14:18 -0300

    Muda o ip da api

--- Client/js/Factories/api.js
+++ Client/js/Factories/api.js
@@ -1,7 +1,7 @@
 app.factory('api', function () {
 
     return function (url) {
-        return 'http://52.39.66.114:8000/api/' + url;
+        return 'http://localhost:8000/api/' + url;
     }
 
 });
